8dfe266a2d4827c6353e930002bbca2306239672
Manh Vu <vdmanh107@gmail.com>
2018-07-25 09:25:07 +0700

    Add recipe

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,3 +1,12 @@
 class RecipeIngredient < ApplicationRecord
-  belongs_to :recipe
+  attr_accessor :temp
+
+  before_save :split
+
+  belongs_to :recipe, class_name: Recipe.name
+
+  def split
+    self.amount, self.measurement, self.ingredient_name,
+       = temp.split(" ", Settings.ingredient_split)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_07_27_062443) do
   end
 
   create_table "recipe_ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.string "amount"
     t.string "measurement"
     t.datetime "created_at", null: false
