367fd0beac213543934919c807891e902a7276b7
Manh Vu <vdmanh107@gmail.com>
2018-07-31 15:42:14 +0700

    Add, destroy comment with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,36 @@
 class CommentsController < ApplicationController
   before_action :correct_user, only: :destroy
+  before_action :find_user, only: %i(create, destroy)
 
   def new; end
 
   def create
     @comment = Comment.new comment_params
+
     if @comment.save
-      flash[:success] = t ".added"
+      respond_to do |format|
+        format.html {redirect_to @recipe}
+        format.js {render layout: false}
+      end
     else
-      flash[:warning] = t ".error"
+      render js: "alert(I18n.t('empty_comment_alert'))"
     end
-    redirect_to @comment.recipe
   end
 
   def destroy
-    @recipe = Recipe.find_by id: params[:id]
-    @comment = @recipe.comment
+    @comment = @recipe.comments.find_by id: params[:id]
     @comment.destroy
-    redirect_to recipe_path @recipe
+    respond_to do |format|
+      format.html {redirect_to recipe_path @recipe}
+      format.js
+    end
   end
 
   private
+
+  def find_recipe
+    @recipe = Recipe.find_by id: params[:recipe_id]
+  end
 
   def comment_params
     params.require(:comment).permit(:content,
@@ -29,6 +39,6 @@ class CommentsController < ApplicationController
 
   def correct_user
     @comment = current_user.comments.find_by id: params[:id]
-    redirect_to root_url unless @micropost
+    redirect_to root_url unless @comment
   end
 end
