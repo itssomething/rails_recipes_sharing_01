85c502ef244d0fa962f5c2a39fade8d4c185e48b
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-08-01 20:16:46 +0700

    Dynamic ingredient form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,18 +9,32 @@
 //= require i18n/translations
 
 $(document).on("turbolinks:load", function() {
-
-  $("form").on("click", ".add_fields", function(event) {
+  $("form").on("click", ".add_step_fields", function(event) {
     var regexp, time;
     time = new Date().getTime();
     regexp = new RegExp($(this).data("id"), "g");
-    $(".fields").append($(this).data("fields").replace(regexp, time));
+    $(".step_fields").append($(this).data("fields").replace(regexp, time));
     return event.preventDefault() ;
   });
 
-  $("form").on("click", ".remove_record", function(event) {
+  $("form").on("click", ".remove_step_record", function(event) {
     $(this).prev("input[type=hidden]").val("1");
     $(this).closest("div.step").hide();
+    return event.preventDefault() ;
+
+  });
+
+  $("form").on("click", ".add_ingredient_fields", function(event) {
+    var regexp, time;
+    time = new Date().getTime();
+    regexp = new RegExp($(this).data("id"), "g");
+    $(".ingredient_fields").append($(this).data("fields").replace(regexp, time));
+    return event.preventDefault() ;
+  });
+
+  $("form").on("click", ".remove_ingredient_record", function(event) {
+    $(this).prev("input[type=hidden]").val("1");
+    $(this).closest("div.ingredient").hide();
     return event.preventDefault() ;
 
   });
